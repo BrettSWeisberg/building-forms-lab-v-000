54080a37e60e25cd5ef5096b18983a75ef634282
github username <github email address>
2019-03-10 18:15:58 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,46 @@
 // Add BandInput component
 import React, { Component } from 'react'
+//import { connect } from 'react-redux';
 
 class BandInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+  event.preventDefault();
+  this.props.addBand(this.state.name)
+  this.props.dispatch({ type: 'ADD_BAND', payload: this.state });
+}
+
   render() {
     return(
       <div>
         Band Input
+          <form onSubmit={event => this.handleSubmit(event)}>
+           <p>
+             <label>Brand Name</label>
+             <input
+            type="text"
+            onChange={this.handleChange} value={this.state.text}/>
+           </p>
+            <input type="submit" />
+          </form>
       </div>
     )
   }
 }
 
-export default BandInput
+
+
+export default BandInput;
+//export default connect(null, mapDispatchToProps)(BandInput);
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput'
 
 class BandsContainer extends Component {
+
+
+   //renderBands = () => this.props.bands.map((band, id) => <div key={id}>{band.name}</div>)
+  //{this.renderBands}
+
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand} />
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  return {
+    bands: state.bands
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addBand: formData => dispatch({ type: 'ADD_BAND', formData })
+    };
+};
+
+export default connect(mapStateToProps)(BandsContainer);
